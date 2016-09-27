db5eb989c57ad7c9ba7deb5f561ba8e075f51ef7
donggeun.kim <rogue22@hanmail.net>
2016-09-27 14:14:47 +0900

    Main Toolbar 추가

--- app/src/main/java/com/questcompany/mustlist/MainActivity.java
+++ app/src/main/java/com/questcompany/mustlist/MainActivity.java
@@ -1,8 +1,15 @@
 package com.questcompany.mustlist;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by kimkkikki on 2016. 9. 26..
@@ -10,9 +17,60 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private static final String TAG = "MainActivity";
+
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        if (toolbar != null) {
+            toolbar.setTitle("");
+            setSupportActionBar(toolbar);
+        } else {
+            Log.e(TAG, "onCreate: toolbar is null");
+        }
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.app_name, R.string.app_name);
+        if (drawerLayout != null) {
+            drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        } else {
+            Log.e(TAG, "onCreate: drawerLayout is null");
+        }
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        } else {
+            Log.e(TAG, "onCreate: getSupportActionBar is null");
+        }
+    }
+
+    // Tool Bar
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return actionBarDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 }
